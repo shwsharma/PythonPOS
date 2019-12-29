ba351339d6a6e8aa6b52bba001f719fc395fc909
Varun <varun.cg@gmail.com>
2019-12-29 18:04:29 +0530

    authorize users based role

--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from store.models import ProductCategory, Product
 from store.serializers import ProductCategorySerializer, ProductSerializer
 
@@ -10,8 +10,22 @@ class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     authentication_classes = [BasicAuthentication]
 
+    def get_permissions(self):
+        if self.action == 'list':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
+
 
 class ProductCategoryViewSet(viewsets.ModelViewSet):
     queryset = ProductCategory.objects.all()
     serializer_class = ProductCategorySerializer
     authentication_classes = [BasicAuthentication]
+
+    def get_permissions(self):
+        if self.action == 'list':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
