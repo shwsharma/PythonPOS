3c31925fe407bff33c1a780b73095c70ee8553c5
Varun <varun.cg@gmail.com>
2019-12-29 11:54:45 +0530

    added product models

--- pos/urls.py
+++ pos/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('store/', include('store.urls'))
+    path('store/', include('store.urls')),
+    path('api-auth/', include('rest_framework.urls'))
 ]
--- store/models.py
+++ store/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField()
+    category = models.ForeignKey(to="ProductCategory", on_delete=models.CASCADE)
+    unit_price = models.FloatField()
+    quantity_available = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
